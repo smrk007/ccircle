d9fdf3a10316d9fe4a4b38d782b3eaa400fd22ee
smrk007 <skenne5@tigers.lsu.edu>
2017-07-09 13:37:22 -0500

    7-9-2017 1:36 PM

--- 2D MineCraft/main.py
+++ 2D MineCraft/main.py
@@ -1,10 +1,38 @@
 import ccircle
 import player
-import tile
+from abc import *
 
 window = ccircle.Window()
 
+'''
+Classes:
 
+Tile 
+noneTile
+dirtTile
+'''
+class Tile:
+    __metaclass__ = ABCMeta
+
+    def __init__(self, posX, posY):
+        self.size = 20
+        self.posX = posX
+        self.posY = posY
+        self.health = 100
+        self.color = (0,0,0)
+        self.solid = False
+
+    @abstractmethod
+    def draw(self):
+        window.draw(self.posX*self.size, self.posY*self.size, self.size, self.size, self.color[0], self.color[1], self.color[2])
+class noneTile(Tile):
+    def __init__(self, posX, posY):
+        super().__init__(posX, posY)
+class dirtTile(Tile):
+    def __init__(self, posX, posY):
+        super().__init__(posX, posY)
+        self.solid = True
+        self.color = (1,1,1)
 
 # World Initialization
 worldSize = 100
@@ -12,8 +40,13 @@ world = []
 for i in range(worldSize):
     row = []
     for j in range(worldSize):
-        row.append(None)
+        if i < 5:
+            row.append(noneTile(i,j))
+        else:
+            row.append(dirtTile(i,j))
     world.append(row)
+
+
 
 
 while window.isOpen():
@@ -24,9 +57,7 @@ while window.isOpen():
 
 
     # Graphics
-    for tile in environment:
+    for tile in world:
         tile.draw()
-
-    player.draw
 
     window.update()
