9b6e51076d799601542904b3876406b7a51aab79
smrk007 <skenne5@lsu.edu>
2017-07-09 14:10:42 -0500

    7-9-2017 2:10 PM

--- 2D MineCraft/main.py
+++ 2D MineCraft/main.py
@@ -1,8 +1,27 @@
 import ccircle
-import player
 from abc import *
 
 window = ccircle.Window()
+
+'''
+Functions:
+
+keyDown
+'''
+
+def keyDown(): # Returns string of whatever key is down, None if no key
+
+    letters = 'abcdefghijklmnopqrstuvwxyz'
+    for char in letters:
+        if ccircle.isKeyDown(char):
+            return char
+
+    etc = ['backspace','enter','esc','space','left','right','up','down']
+    for key in etc:
+        if ccircle.isKeyDown(key):
+            return key
+
+    return None # If no key is down
 
 '''
 Classes:
@@ -10,12 +29,14 @@ Classes:
 Tile 
 noneTile
 dirtTile
+
+player
 '''
 class Tile:
     __metaclass__ = ABCMeta
 
     def __init__(self, posX, posY):
-        self.size = 20
+        self.size = 30
         self.posX = posX
         self.posY = posY
         self.health = 100
@@ -24,7 +45,7 @@ class Tile:
 
     @abstractmethod
     def draw(self):
-        window.draw(self.posX*self.size, self.posY*self.size, self.size, self.size, self.color[0], self.color[1], self.color[2])
+        window.drawRect(self.posX*self.size, self.posY*self.size, self.size, self.size, self.color[0], self.color[1], self.color[2])
 class noneTile(Tile):
     def __init__(self, posX, posY):
         super().__init__(posX, posY)
@@ -34,19 +55,39 @@ class dirtTile(Tile):
         self.solid = True
         self.color = (1,1,1)
 
+class Player:
+
+    def __init__(self, posX, posY):
+        self.posX = posX
+        self.posY = posY
+        self.velX = 0
+        self.velY = 0
+        self.color = (1,1,1)
+
+    def move(self, key):
+        self.velX = 0
+        self.velY = 0
+        if key == 'left':
+            self.velX = -0.5
+        if key == 'right':
+            self.velX = 0.5
+
+    def draw(self):
+        window.drawCircle(self.posX, self.posY, 15, self.color[0], self.color[1], self.color[2])
+
 # World Initialization
-worldSize = 100
+worldSize = 20
 world = []
 for i in range(worldSize):
     row = []
     for j in range(worldSize):
-        if i < 5:
-            row.append(noneTile(i,j))
+        if i < 10:
+            row.append(noneTile(j,i))
         else:
-            row.append(dirtTile(i,j))
+            row.append(dirtTile(j,i))
     world.append(row)
 
-
+player = Player(30,30)
 
 
 while window.isOpen():
@@ -55,9 +96,14 @@ while window.isOpen():
 
     # Physics and
 
+    player.move(keyDown())
+    player.posX += player.velX
+    player.posY += player.velY
 
     # Graphics
-    for tile in world:
-        tile.draw()
+    for layer in world:
+        for tile in layer:
+            tile.draw()
+    player.draw()
 
     window.update()
